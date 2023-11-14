a713c4103cbc6cef5bcc51c64893e40328de94ad
kangfenmao <kangfenmao@qq.com>
2023-11-14 14:17:49 +0800

    feat(indexer): add file.go with utility functions
    - Add CalculateFileHash function to calculate the MD5 hash of a file.
    - Add IsFileContentChanged function to check if the content of a file has changed.
    - Add ListTextFiles function to list all text files (.txt and .md) in a directory.
    - Add CreateDirectoryIfNotExists function to create a directory if it doesn't exist.
    
    Repository: /Users/wangqian/Code/github/vortex-notes
    
    Issue: main

--- indexer/drivers/local_indexer.go
+++ indexer/drivers/local_indexer.go
@@ -2,9 +2,7 @@ package drivers
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+	"vortex-notes/indexer/utils"
 )
 
 type LocalIndexer struct {
@@ -13,73 +11,31 @@ type LocalIndexer struct {
 func (local LocalIndexer) ListAllNotes() []string {
 	const path = "./data/vortexnotes"
 
-	err := createDirectoryIfNotExists(path)
+	err := utils.CreateDirectoryIfNotExists(path)
 	if err != nil {
 		var list []string
 		fmt.Println("Error:", err)
 		return list
 	}
 
-	var notes, _ = listTextFiles(path)
+	var notes, _ = utils.ListTextFiles(path)
 	return notes
 }
 
 func (local LocalIndexer) IndexExist(path string) bool {
-	fmt.Println("IndexExist: ", path)
 	return false
 }
 
 func (local LocalIndexer) ParseNote(path string) string {
-	fmt.Println("ParseNote: ", path)
 	return ""
 }
 
 func (local LocalIndexer) AddNoteToIndex(path string, note string) bool {
-	fmt.Println("AddNoteToIndex: ", path)
+	hash, err := utils.CalculateFileHash(path)
+	if err != nil {
+		return true
+	}
+
+	fmt.Println("AddNoteToIndex: ", path, hash)
 	return true
-}
-
-func listTextFiles(dirPath string) ([]string, error) {
-	var textFiles []string
-
-	// 打开目录
-	dir, err := os.Open(dirPath)
-	if err != nil {
-		return nil, err
-	}
-	defer dir.Close()
-
-	fileInfos, err := dir.Readdir(-1)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, fileInfo := range fileInfos {
-		if fileInfo.IsDir() {
-			continue
-		}
-
-		ext := strings.ToLower(filepath.Ext(fileInfo.Name()))
-
-		if ext == ".txt" || ext == ".md" {
-			filePath := filepath.Join(dirPath, fileInfo.Name())
-			textFiles = append(textFiles, filePath)
-		}
-	}
-
-	return textFiles, nil
-}
-
-func createDirectoryIfNotExists(dirPath string) error {
-	_, err := os.Stat(dirPath)
-	if os.IsNotExist(err) {
-		err := os.MkdirAll(dirPath, os.ModePerm)
-		if err != nil {
-			return err
-		}
-	} else if err != nil {
-		return err
-	}
-
-	return nil
 }
---
+++ indexer/utils/file.go
@@ -0,0 +1,74 @@
+package utils
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func CalculateFileHash(filePath string) (string, error) {
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	hash := md5.Sum(fileData)
+	return fmt.Sprintf("%x", hash), nil
+}
+
+func IsFileContentChanged(filePath string, previousHash string) (bool, error) {
+	currentHash, err := CalculateFileHash(filePath)
+	if err != nil {
+		return false, err
+	}
+
+	return currentHash != previousHash, nil
+}
+
+func ListTextFiles(dirPath string) ([]string, error) {
+	var textFiles []string
+
+	// 打开目录
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, fileInfo := range fileInfos {
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		ext := strings.ToLower(filepath.Ext(fileInfo.Name()))
+
+		if ext == ".txt" || ext == ".md" {
+			filePath := filepath.Join(dirPath, fileInfo.Name())
+			textFiles = append(textFiles, filePath)
+		}
+	}
+
+	return textFiles, nil
+}
+
+func CreateDirectoryIfNotExists(dirPath string) error {
+	_, err := os.Stat(dirPath)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(dirPath, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	return nil
+}
