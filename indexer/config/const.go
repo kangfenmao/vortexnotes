68b817cfe77f47cc89230d7528a1d66553954f4c
kangfenmao <kangfenmao@qq.com>
2023-11-15 16:54:06 +0800

    feat(const): Update MeiliSearch API key and remove unnecessary environment variable (#main)

--- indexer/config/const.go
+++ indexer/config/const.go
@@ -7,9 +7,7 @@ const AppDataPath = "data/vortexnotes/"
 const AppDbPath = AppDataPath + "app.db"
 const NotesJsonFilePath = AppDataPath + "notes.json"
 const MeiliSearchHost = "http://localhost:7700"
-const MeiliSearchAPIKey = "zXEpbeyeGtGi8DQbfOSALKywwr982pQaROL6rBwAK35wCAv6ZsdIBexLzyDVKlm9"
 
 var MeiliSearchClient = meilisearch.NewClient(meilisearch.ClientConfig{
-	Host:   MeiliSearchHost,
-	APIKey: MeiliSearchAPIKey,
+	Host: MeiliSearchHost,
 })
