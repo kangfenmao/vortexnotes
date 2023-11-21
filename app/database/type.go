1e5871a200f17cc4e05e18cf64846a9ce315b6b0
kangfenmao <kangfenmao@qq.com>
2023-11-21 16:00:22 +0800

    feat(database): add NoteModel struct in type.go (main)

--- app/database/type.go
+++ app/database/type.go
@@ -1,6 +1,6 @@
 package database
 
-type Note struct {
+type NoteModel struct {
 	ID      string `gorm:"primaryKey" json:"id"`
 	Name    string `json:"name"`
 	Content string `json:"content"`
---
+++ app/types/type.go
@@ -0,0 +1,7 @@
+package types
+
+type NoteDocument struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
