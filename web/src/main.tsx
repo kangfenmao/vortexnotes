246b61b48669fcafa4286982a0e7b83a10b5bac9
kangfenmao <kangfenmao@qq.com>
2023-11-21 10:11:52 +0800

    feat(web): remove strict mode from ReactDOM rendering (main)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import '@/config/http.ts'
 import App from '@/App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
